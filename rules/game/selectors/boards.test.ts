c88d3bc55b763475408fd46a039e664c83a31831
Mike Hildebrand-Faust <michaelhildebrand@lithia.com>
2023-03-10 09:35:06 -0800

    test that you cant do an en passant capture when you should not be able to

--- rules/game/selectors/boards.test.ts
+++ rules/game/selectors/boards.test.ts
@@ -20,7 +20,7 @@ describe('boards', () => {
   it('gets Boards[] from a simple first move', () => {
     const gameState: GameState = {
       history: 'E2-E4,E7-E5',
-      boardCursor: 1,
+      boardCursor: 1,//<-- doesn't matter
     };
     const gameBoards = boards(gameState);
 
@@ -28,14 +28,22 @@ describe('boards', () => {
     expect(gameBoards.length).toBe(3);
   });
 
-  it.only('Recognizes en-passant capture', () => {
+  it('Recognizes en-passant capture', () => {
     const gameState: GameState = {
       history: 'E2-E4,G8-H6,E4-E5,F7-F5,E5-F6',
-      boardCursor: 1,
+      boardCursor: 1,//<-- doesn't matter
     };
     const gameBoards = boards(gameState);
 
     gameBoards.forEach(b => console.log(textRender(b)))
     expect(gameBoards.length).toBe(6);
+  });
+
+  it(`Doesn't allow en-passant capture on moves too late`, () => {
+    const gameState: GameState = {
+      history: 'E2-E4,G8-H6,E4-E5,F7-F5,F1-E2,G7-E6,E5-F6',
+      boardCursor: 1,//<-- doesn't matter
+    };
+    expect(() => boards(gameState)).toThrow()
   });
 })
