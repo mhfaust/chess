781d666341f3f154643ac51c7b37350dc782855d
Mike Hildebrand-Faust <michaelhildebrand@lithia.com>
2023-03-15 14:56:34 -0700

    remove board annotations stuff

--- logic/board/index.ts
+++ logic/board/index.ts
@@ -2,6 +2,4 @@ export { default as rotateCounterClockwise } from './rotateCounterClockwise';
 export { default as emptyBoard } from './emptyBoard';
 export { default as initialBoard } from './initialBoard';
 export { default as move } from './move';
-export { default as nextBoardAnnotations } from './nextBoardAnnotations';
-export { default as initialBoardAnnotations } from './initialBoardAnnotations';
 export { default as nextCastlingPreclusions } from './nextCastlingPreclusions';
