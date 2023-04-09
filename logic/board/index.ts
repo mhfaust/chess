76faac8529f20db66565fbca161b2ea5874e82d9
Mike Hildebrand-Faust <michaelhildebrand@lithia.com>
2023-04-09 12:22:57 -0700

    cleanup

--- logic/board/index.ts
+++ logic/board/index.ts
@@ -1,4 +1,3 @@
-export { default as emptyBoard } from './emptyBoard';
 export { initialBoard } from './initialBoard';
 export { default as move } from './move';
 export { default as nextCastlingPreclusions } from './nextCastlingPreclusions';
