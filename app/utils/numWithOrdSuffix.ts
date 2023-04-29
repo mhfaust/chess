ab1a6a307c402db4b238318a108a164b1a31dee5
Mike Hildebrand-Faust <michaelhildebrand@lithia.com>
2023-04-29 15:55:41 -0700

    ord suffix code format and test

--- app/utils/numWithOrdSuffix.ts
+++ app/utils/numWithOrdSuffix.ts
@@ -1,20 +1,16 @@
 const teens = new Map([
-  [11, 'th'],
-  [12, 'th'],
-  [13, 'th'],
+  [11, 'th'], [12, 'th'], [13, 'th'],
 ]);
 const lastDigit = new Map([
-  [1, 'st'],
-  [2, 'nd'],
-  [3, 'rd']
+  [1, 'st'], [2, 'nd'], [3, 'rd']
 ])
 
 const ordinalSuffix = (n: number) => {
   return teens.get(n % 100) || lastDigit.get(n % 10) || 'th';
 }
 
-const numWithOrdSuffix = (n: number | string) => {
+const numberWithOrdSuffix = (n: number | string) => {
   return n + ordinalSuffix(parseInt(n as string, 10));
 }
 
-export default numWithOrdSuffix;
+export default numberWithOrdSuffix;
