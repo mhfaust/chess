2356748c686852616eb18ca6048ea3e61fb895cf
Mike Hildebrand-Faust <mhfaust@gmail.com>
2023-11-28 22:14:19 -0800

    auth setup + wrap in server fns

---
+++ app/auth.ts
@@ -0,0 +1,34 @@
+import NextAuth from "next-auth";
+import Github from 'next-auth/providers/github'
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from 'app/db'
+
+const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = process.env
+
+if(!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
+  throw Error('Missing github OAuth Credentials')
+}
+
+export const { 
+  handlers: { GET, POST }, 
+  auth, 
+  signOut, 
+  signIn 
+} = NextAuth({
+  adapter: PrismaAdapter(db),
+  providers: [
+    Github({
+      clientId: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET
+    })
+  ],
+  callbacks: {
+    //Usually not needed, here we are fixing a bug in nextauth
+    async session({ session, user }) {
+      if(session?.user && user) {
+        session.user.id = user.id
+      }
+      return session
+    }
+  }
+})
