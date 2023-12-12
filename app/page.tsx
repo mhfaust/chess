c0167bdb43d7fcc75ee8a640e1fd3f9c26023f03
Mike Hildebrand-Faust <mhfaust@gmail.com>
2023-12-11 20:43:09 -0800

    remov extra provider instance. Delete unused css files

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
-import Providers from 'app/_components/Providers';
 import { createGame } from 'app/serverActions';
 
 export default function Home() {
 	return (
-		<Providers>
-			<div>
-				<form action={createGame}>
-					<button type='submit'>Play</button>
-				</form>
-			</div>
-		</Providers>
+		<div>
+			<form action={createGame}>
+				<button type='submit'>Play</button>
+			</form>
+		</div>
 	);
 }
